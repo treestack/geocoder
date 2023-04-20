69cbdfe5d56737c26f26d0c604f171d0c5d6448e
Christoph von Krüchten <cvk@treestack.de>
2023-04-20 09:05:26 +0200

    fix bench

--- geocoder/benches/geocoder_bench.rs
+++ geocoder/benches/geocoder_bench.rs
@@ -5,11 +5,11 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use geocoder::ReverseGeocoder;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let gc = ReverseGeocoder::new("./cities.csv");
+    let gc = ReverseGeocoder::from_file("../cities500.txt");
     let lat: f32 = 47.1;
     let lng: f32 = 11.0;
     c.bench_function("geocode 47.1/11.0", |b| {
-        b.iter(|| gc.search(black_box(&lat), black_box(&lng)))
+        b.iter(|| gc.search(black_box(&lat), black_box(&lng), black_box(&1)))
     });
 }
 
