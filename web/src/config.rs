f2cdbaeec4c3f727c5b5e3170aa687b7f0026b26
Christoph von Krüchten <cvk@treestack.de>
2023-04-21 09:50:38 +0200

    use envy for configuration

--- web/src/config.rs
+++ web/src/config.rs
@@ -1,37 +1,51 @@
 use crate::errors::Error::ConfigurationError;
 use crate::Result;
-use std::env;
+use serde::Deserialize;
+use serde_with::{serde_as, DisplayFromStr};
 use std::net::SocketAddr;
-use std::ops::Deref;
-use std::str::FromStr;
 use tracing::Level;
 
+#[serde_as]
+#[derive(Deserialize, Debug)]
 pub struct Configuration {
+    #[serde_as(as = "DisplayFromStr")]
+    #[serde(default = "default_loglevel")]
     pub loglevel: Level,
+    #[serde(default = "default_bind_address")]
     pub bind_address: SocketAddr,
+    #[serde(default = "default_data_file")]
     pub data_file: String,
+    #[serde(default = "default_quota_burst_size")]
+    pub quota_burst_size: u32,
+    #[serde(default = "default_quota_interval")]
+    pub quota_interval: u64,
+    #[serde(default = "default_watch_for_changes")]
+    pub watch_for_changes: bool,
+}
+
+fn default_loglevel() -> Level {
+    Level::INFO
+}
+fn default_bind_address() -> SocketAddr {
+    SocketAddr::from(([127, 0, 0, 1], 5353))
+}
+fn default_data_file() -> String {
+    String::from("./cities500.txt")
+}
+fn default_quota_burst_size() -> u32 {
+    10
+}
+fn default_quota_interval() -> u64 {
+    1000
+}
+fn default_watch_for_changes() -> bool {
+    true
 }
 
 impl Configuration {
     pub fn from_env() -> Result<Configuration> {
-        let default = Self::default();
-        Ok(Self {
-            loglevel: Level::from_str(env::var("LOGLEVEL")?.deref()).unwrap_or(default.loglevel),
-            bind_address: env::var("BIND_ADDRESS")
-                .map_err(ConfigurationError)
-                .and_then(|v| Ok(SocketAddr::from_str(&v)?))
-                .unwrap_or(default.bind_address), //TODO
-            data_file: env::var("DATA_FILE").unwrap_or(default.data_file),
-        })
-    }
-}
-
-impl Default for Configuration {
-    fn default() -> Self {
-        Self {
-            loglevel: Level::INFO,
-            bind_address: SocketAddr::from(([127, 0, 0, 1], 5353)),
-            data_file: String::from("./cities500.txt"),
-        }
+        envy::prefixed("GEOCODER_")
+            .from_env::<Configuration>()
+            .map_err(ConfigurationError)
     }
 }
--- web/src/errors.rs
+++ web/src/errors.rs
@@ -1,15 +1,10 @@
 use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
-use std::env::VarError;
-use std::net::AddrParseError;
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
     #[error("invalid configuration: {0}")]
-    ConfigurationError(#[from] VarError),
-
-    #[error("invalid configuration: {0}")]
-    ParseError(#[from] AddrParseError),
+    ConfigurationError(#[from] envy::Error),
 }
 
 impl IntoResponse for Error {
@@ -19,6 +14,7 @@ impl IntoResponse for Error {
     }
 }
 
+#[allow(unreachable_patterns)]
 fn to_response_tuple(err: Error) -> (StatusCode, String) {
     match err {
         Error::ConfigurationError(_) => (StatusCode::NOT_FOUND, err.to_string()),
